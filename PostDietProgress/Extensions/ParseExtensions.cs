788f07de18b6159f04ab12e7f37602f95f5b7d07
Ovis <me+github@hitsujin.jp>
2020-04-09 10:58:21 +0900

    数値パース用拡張追加

---
+++ PostDietProgress/Extensions/ParseExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace PostDietProgress.Extensions
+{
+    public static class ParseExtensions
+    {
+        public static double? ToDoubleOrNull(this string str)
+        {
+            if (double.TryParse(str, out var result))
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public static long? ToLongOrNull(this string str)
+        {
+            if (long.TryParse(str, out var result))
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
